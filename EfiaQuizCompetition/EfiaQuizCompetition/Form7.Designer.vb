7914cefaf87643dad9fe749117f6426941d846f2
efiaquizapp <stofrislot@throwam.com>
2016-11-07 22:46:16 +0400

    Bala's Computer [Two-Way Test]

--- EfiaQuizCompetition/EfiaQuizCompetition/Form7.Designer.vb
+++ EfiaQuizCompetition/EfiaQuizCompetition/Form7.Designer.vb
@@ -22,16 +22,29 @@ Partial Class Form7
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(494, 627)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Correct"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form7
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1902, 1033)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "Form7"
         Me.Text = "Form7"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents Button1 As Button
 End Class
